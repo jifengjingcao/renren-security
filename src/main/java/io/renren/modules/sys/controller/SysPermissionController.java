ccebb61ee6fb79cb6e708c7703787eeac4603246
wuzhifeng <839300274@qq.com>
2017-11-20 17:56:01 +0800

    提交

---
+++ src/main/java/io/renren/modules/sys/controller/SysPermissionController.java
@@ -0,0 +1,16 @@
+package io.renren.modules.sys.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 权限管理
+ * Created by wzf on 2017/11/20.
+ */
+
+@RestController
+@RequestMapping("/sys/permission")
+public class SysPermissionController extends AbstractController{
+
+
+}
